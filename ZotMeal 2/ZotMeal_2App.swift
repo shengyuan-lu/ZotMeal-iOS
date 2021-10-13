88cf0f5d5a525eff236506d13127efe6d498875e
Shengyuan Lu <shengyuan.lu.developer@gmail.com>
2021-10-12 22:11:31 -0700

    Initial Commit

---
+++ ZotMeal 2/ZotMeal_2App.swift
@@ -0,0 +1,17 @@
+//
+//  ZotMeal_2App.swift
+//  ZotMeal 2
+//
+//  Created by Shengyuan Lu on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ZotMeal_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
