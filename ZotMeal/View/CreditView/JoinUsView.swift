33f1307515382ab777d2a23fa6af31edf13c3b59
Jon <jwlow@uci.edu>
2022-04-04 01:12:14 -0700

    Added email for application inquiries
    Was super sugon on applying

--- ZotMeal/View/CreditView/JoinUsView.swift
+++ ZotMeal/View/CreditView/JoinUsView.swift
@@ -19,7 +19,7 @@ struct JoinUsView: View {
                     
                     Text("Stop putting projects you did in ICS 32 or 45C on your resume. It's time to show something real to your employers. ZotMeal is not possible without crazily talented people at UC Irvine. Do you want to make a dent in the universe? By joining our team, your work will have real users: your peers, staffs, or even professors. This feeling is insanely great.")
                     
-                    Text("ZotMeal runs on a total ownership / responsibility model. That means your work belongs to you. What you write stays in your personal portfolio and your own GitHub. This is very different from ICSSC or CTC which your work ultimately boosted the club leader's resume. Also there's no title like 'president' or 'vp' or 'manager'. We are a flat organization and we take pride in giving everyone credit and a platform to show their skills.")
+                    Text("ZotMeal runs on a total ownership / responsibility model. That means your work belongs to you. What you write stays in your personal portfolio and your own GitHub. This is very different from ICSSC or CTC which your work ultimately boosted the club leader's resume. Also there's no title like 'president' or 'vp' or 'manager'. We are a flat organization and we take pride in giving everyone credit and a platform to show their skills. For those interested in applying please contact shengyuan.lu.developer@gmail.com")
                     
                     
                     Spacer()
